28da1d888ca98349ccb7d8c6b54f21057adad57e
kritsawan <kritsawan.pi@baac.or.th>
2015-10-19 15:49:34 +0700

    เตรียมดาต้าเบสเสร็จแล้ว

--- app/src/main/java/baac/pi/kritsawan/baacrestaurant/MainActivity.java
+++ app/src/main/java/baac/pi/kritsawan/baacrestaurant/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //Explicit
+
+    //step1
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;//สีเทาไม่ได้ใช้งาน สีม่วงถูกเอาไปใช้งาน
 
 
     @Override
@@ -16,11 +19,21 @@ public class MainActivity extends AppCompatActivity {
         //create & connected Database
         createAndConnect();
 
+        //Tester Add New Values
+        testerAdd();
+
     }// Main method
 
-    private void createAndConnect() {
+    private void testerAdd() {
+        //step2
+        objUserTABLE.addNewUser("testUser", "testPass", "ทดสอบไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร","testSource","123");
+    }
 
+    private void createAndConnect() {
+        //step3
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
 
 
     }
--- app/src/main/java/baac/pi/kritsawan/baacrestaurant/UserTABLE.java
+++ app/src/main/java/baac/pi/kritsawan/baacrestaurant/UserTABLE.java
@@ -1,5 +1,6 @@
 package baac.pi.kritsawan.baacrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,7 +11,13 @@ public class UserTABLE {
 
     //Explicit
     private MyOpenHelper objMyOpenHelper;
-    private SQLiteDatabase WriteSqLiteDatabase, readSqLiteDatabase;
+    private SQLiteDatabase WriteSqLiteDatabase,readSqLiteDatabase;
+
+    public static final String USER_TABLE = "userTABLE";
+    public static final String COLUMN_ID_USER = "_id";
+    public static final String COLUMN_USER = "User";
+    public static final String COLUMN_PASSWORD = "Password";
+    public static final String COLUMN_NAME = "Name";
 
     public UserTABLE(Context context) {
 
@@ -19,4 +26,14 @@ public class UserTABLE {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }//constructor
+
+    public long addNewUser(String strUser, String strPassword, String strName){
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_USER, strUser);
+        objContentValues.put(COLUMN_PASSWORD, strPassword);
+        objContentValues.put(COLUMN_NAME, strName);
+        return readSqLiteDatabase.insert(USER_TABLE, null, objContentValues);
+        //return 0;
+    }
 }
