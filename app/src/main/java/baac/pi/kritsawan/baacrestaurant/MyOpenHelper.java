84c24dd1da101e236ab7ddd53e0cc24d28f8422e
kritsawan <kritsawan.pi@baac.or.th>
2015-10-19 13:05:15 +0700

    เริ่มใช้งาน GitHub

---
+++ app/src/main/java/baac/pi/kritsawan/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,19 @@
+package baac.pi.kritsawan.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2558.
+ */
+public class MyOpenHelper {
+
+    //Explicit ประกาศตัวแปรเพื่อให้โทรศัพท์คำนวนเมมถ้าพอให้รันได้เลย
+    private static final String DATABASE_NAME ="BAAC.db";//เปลี่ยนแปลงไม่ได้
+    private static final int DATABASE_VERSION = 1;
+
+
+
+
+    public MyOpenHelper(Context context) {
+    }// Constructor
+}//Main Class
